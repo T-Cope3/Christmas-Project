0d0c85a515dff57114397c479b3cf8c9eaf0dea6
BattleMittens <battlemitten@gmail.com>
2017-12-08 20:39:02 -0500

    Creation of Master Java File
    This is the file that we will be doing all of our coding in. I created it today just to be worked on at any time. Please ignore the package part, it's just for Eclipse.

---
+++ ChristmasGroupProject.java
@@ -0,0 +1,14 @@
+//ignore the package thing, it's just so that I can pull it up in eclipse without a problem.
+package com.Troy_Test.Troy;
+
+import java.util.*;
+
+public class ChristmasGroupProject 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
