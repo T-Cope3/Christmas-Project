318eb296523e5860ecdbe6bffe943e859e7caae5
BattleMittens <battlemitten@gmail.com>
2017-12-10 11:58:57 -0500

    Addition of mandatory variables
    Creation of simplistic variables that can be changed at any time. It's just a simple concept, which is not finalized.

--- ChristmasGroupProject.java
+++ ChristmasGroupProject.java
@@ -8,7 +8,26 @@ public class ChristmasGroupProject
 
 	public static void main(String[] args) 
 	{
+		//Main Variables
+		int Totalbudget = 0;
+		int budgetLeft = 0;
+		//don't actually know what data type this should be
+		//Time/Date class might make this an object, or an integer if it's days left for production
+		String daysLeft = " ";
 		
+		//Creation of parallel arrays for the kids:
+		String[] firstName = new String[Integer.MAX_VALUE]; 
+		//could make this boolean if wanted
+		String[] naughtyOrNice = new String[Integer.MAX_VALUE];
+		//might have to parse Integer for the numeric values
+		String[] age = new String[Integer.MAX_VALUE];
+		
+		//Creation of parallel arrays for the presents:
+		String[] giftName = new String[Integer.MAX_VALUE];
+		//could be set up to become an integer, would probably need to
+		String[] giftPrice = new String[Integer.MAX_VALUE];
+		//Could be set to an integer for simplicity: in days
+		String[] giftTime = new String[Integer.MAX_VALUE];
 	}
 
 }
