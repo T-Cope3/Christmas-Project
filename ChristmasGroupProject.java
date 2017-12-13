803ca7a010ce321c8bc44b575f8fa841c47a5b4a
Tyreal Cosrin <battlemitten@gmail.com>
2017-12-13 12:16:05 -0500

    Creation of starter methods and  addtion of daysToMake.
    Adds what has been missing for the arrays, and sets up starter methods to be filled in later.

--- ChristmasGroupProject.java
+++ ChristmasGroupProject.java
@@ -18,7 +18,7 @@ public class ChristmasGroupProject
 		//Creation of parallel arrays for the kids:
 		String[] firstName = new String[Integer.MAX_VALUE];
 		//could make this boolean if wanted
-		String[] naughtyOrNice = new String[Integer.MAX_VALUE];
+		Boolean[] naughtyOrNice = new Boolean[Integer.MAX_VALUE];
 		//might have to parse Integer for the numeric values
 		int[] age = new int[Integer.MAX_VALUE];
 
@@ -28,6 +28,26 @@ public class ChristmasGroupProject
 		double[] giftPrice = new double[Integer.MAX_VALUE];
 		//Could be set to an integer for simplicity: in days
 		String[] giftTime = new String[Integer.MAX_VALUE];
+		//Created for the elves to make the presents
+		int[] daysToMake = new int[Integer.MAX_VALUE];
 	}
 
+	/* -TC-
+	Short Desc: Grabs all the data from the 'kids' file and seporates it by the comma into seporate arrays.
+	Parameters: f; The name of the child, n; whether the kid is naughty or nice [nice = true], a; age of the child.
+	NOTE TO SELF: passed by reference because arrays are objects!
+	*/
+	public static void assignKids(String[] f, Boolean[] n, int[] a)
+	{
+
+	}
+
+	/* -TC-
+	Short Desc: Grabs all the data from the 'kids' file and seporates it by the comma into seporate arrays.
+	Parameters: n; name of the gift, p; the price of the gift, t; how long the gift takes to make, l; the age requirement of the present.
+	*/
+	public static void assignGifts(String[] n, double[] p, String[] t, int[] l)
+	{
+
+	}
 }
