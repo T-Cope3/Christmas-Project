ac98cf877d644e49818a1181e79130ebb9d26775
Isiah Hunter <isia>
2017-12-14 10:42:42 -0500

    Trying
    I cant really add the first names and the NiceORnot to a normall array so i Just made an array list to test to see if im doing things correctly. Not final

--- ChristmasGroupProject.java
+++ ChristmasGroupProject.java
@@ -1,13 +1,20 @@
 //ignore the package thing, it's just so that I can pull it up in eclipse without a problem.
-package com.Troy_Test.Troy;
+//package com.Troy_Test.Troy;
 
 import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.ArrayList;
 
 public class ChristmasGroupProject
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
+
+		//Just used to test my code
+		grabLine();
+
 		//Main Variables
 		int Totalbudget = 0;
 		int budgetLeft = 0;
@@ -28,11 +35,34 @@ public class ChristmasGroupProject
 		double[] giftPrice = new double[Integer.MAX_VALUE];
 		//Could be set to an integer for simplicity: in days
 		String[] giftTime = new String[Integer.MAX_VALUE];
+
+
 	}
 
-	public static String grabLine()
+	public static String grabLine() throws FileNotFoundException
 	{
+		int i = 0;
+		Scanner reader = new Scanner(new File("kids.txt"));
+		//Delimiter with ','
+		reader.useDelimiter(", ");
+		ArrayList<String> firstName = new ArrayList<String>();
+		ArrayList<String> niceORnot = new ArrayList<String>();
+		ArrayList<Integer> age = new ArrayList<Integer>();
+		while(i < 100)
+		{
+			age.add(reader.nextInt());
+			firstName.add(reader.next());
+			niceORnot.add(reader.next());
+			i++;
+		}
+		for(String s: firstName)
+			System.out.println(s);
+		for(String s: niceORnot)
+			System.out.println(s);
+		//Close Scanner
+		reader.close();
 
+		return ",";
 	}
 
 }
