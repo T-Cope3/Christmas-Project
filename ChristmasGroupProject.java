3ecb23d61aeb0239c4b380123c455f162684db0a
John J Hayes <34247425+ChefKef@users.noreply.github.com>
2017-12-15 11:54:46 -0500

    This is the logic for balancing the budget.
    I will be adding the elimination of the naughty children next.

--- ChristmasGroupProject.java
+++ ChristmasGroupProject.java
@@ -30,4 +30,63 @@ public class ChristmasGroupProject
 		String[] giftTime = new String[Integer.MAX_VALUE];
 	}
 
+	//Begin main1
+	public static void main1(String[] args)
+	{
+		//Declare variables
+		int kids = 25, counter = 24;
+		double budget = 240.00;
+		String[] names = new String[kids];
+		double[] giftPrice = new double[kids];
+		int[] age = new int[kids];
+		boolean[] naughty = new boolean[kids];
+		double gift1 = 10.00, gift2 = 5.00, highestPrice = 0.00;
+		boolean balanced = false, naughtiesCut = false;
+
+		//Begin for 1
+		for(int a = 0; a < 25; a++)
+		{
+			names[a] = ("Holder" + a);
+			age[a] = 13 + (a % 6);
+			naughty[a] = true;
+			//WE WILL NEED TO ORGANIZE PRESENTS FROM HIGHEST TO LOWEST PRICES
+			//In the actual program, this will need to be a for (or maybe a while) loop, that way there is not a massive list of if/else if/else if/...
+			//Begin if/else 1
+			if(budget >= gift1)
+			{
+				giftPrice[a] = gift1;
+				budget = budget - gift1;
+				balanced = true;
+			}
+			else
+			{
+				balanced = false;
+				//Begin while 1
+				while(!balanced)
+				{
+					//Begin if 1
+					if(giftPrice[counter] > gift2 && (budget + (gift1 - gift2)) >= 0.00 && naughtiesCut)
+					{
+						giftPrice[a] = gift2;
+						budget = budget + (gift1 - gift2);
+						balanced = true;
+					}
+					//End if 1
+					counter--;
+				}
+				//End while 1
+				counter = 24;
+			}
+			//End if/else 1
+		}
+		//End for 1
+		//Begin for 3
+		for(int c = 0; c < 25; c++)
+		{
+			System.out.println(names[c] + "is " + age[c] + " years old and got a present worth " + giftPrice[c]);
+		}
+		//End for 3
+		System.out.println(budget);
+	}
+	//End main1
 }
