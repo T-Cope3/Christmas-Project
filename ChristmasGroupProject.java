151f2938b636d07467eb1b390e5961a520fae6cb
Tyreal Cosrin <battlemitten@gmail.com>
2017-12-18 11:25:54 -0500

    Upgrading of primitive data types to their classes.
    Changes to int, and double to become their class objects; Integer and Double.

--- ChristmasGroupProject.java
+++ ChristmasGroupProject.java
@@ -9,8 +9,8 @@ public class ChristmasGroupProject
 	public static void main(String[] args)
 	{
 		//Main Variables
-		int Totalbudget = 0;
-		int budgetLeft = 0;
+		Integer Totalbudget = 0;
+		Integer budgetLeft = 0;
 		//don't actually know what data type this should be
 		//Time/Date class might make this an object, or an integer if it's days left for production
 		String daysLeft = " ";
@@ -20,12 +20,12 @@ public class ChristmasGroupProject
 		//could make this boolean if wanted
 		String[] naughtyOrNice = new String[Integer.MAX_VALUE];
 		//might have to parse Integer for the numeric values
-		int[] age = new int[Integer.MAX_VALUE];
+		Integer[] age = new Integer[Integer.MAX_VALUE];
 
 		//Creation of parallel arrays for the presents:
 		String[] giftName = new String[Integer.MAX_VALUE];
 		//could be set up to become an integer, would probably need to
-		double[] giftPrice = new double[Integer.MAX_VALUE];
+		Double[] giftPrice = new Double[Integer.MAX_VALUE];
 		//Could be set to an integer for simplicity: in days
 		String[] giftTime = new String[Integer.MAX_VALUE];
 	}
